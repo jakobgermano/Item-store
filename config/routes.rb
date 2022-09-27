8242ab6a612ff3b0b58839f05173104137c85170
jakob <jakob.germano@gmail.com>
2022-09-26 20:12:23 -0600

    Added association methods to models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+ resources :items, only: [:index] 
+ resources :users, only: [:index]
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

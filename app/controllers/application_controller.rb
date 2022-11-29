b953d9e1997d26c294d431737faac5d08ea9b319
jakob <jakob.germano@gmail.com>
2022-11-28 21:51:16 -0700

    set up sessions controller and main controller for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_error
+
+before_action :authorize
+
+ def render_not_found_error(error)
+    render json: {error: "record not found"}, status: :not_found
+ end
+
+private
+
+def authorize
+    @current_user = User.find(session[:user_id])
+    render json: {errors: "Not authorized"}, status: unauthorized unless @current_user
+ end
 end
+
